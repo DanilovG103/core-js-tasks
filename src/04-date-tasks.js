6b81f3d82c4b00d44558cb49ed6c3bc71962522f
DanilovG103 <danilov2706@gmail.com>
2021-08-11 15:36:10 +0400

    solved

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -75,8 +75,25 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const date = endDate - startDate;
+  let hour = parseInt(date / 60 / 60 / 1000, 10);
+  let min = parseInt(date / 1000 / 60 - hour * 60, 10);
+  let sec = parseInt(date / 1000 - min * 60 - hour * 3600, 10);
+  let ms = date - 1000 * (hour * 3600 + min * 60 + sec);
+  if (hour.toString().length < 2) {
+    hour = '0'.repeat(2 - hour.toString().length) + hour.toString();
+  }
+  if (min.toString().length < 2) {
+    min = '0'.repeat(2 - min.toString().length) + min.toString();
+  }
+  if (sec.toString().length < 2) {
+    sec = '0'.repeat(2 - sec.toString().length) + sec.toString();
+  }
+  if (ms.toString().length < 3) {
+    ms = '0'.repeat(3 - ms.toString().length) + ms.toString();
+  }
+  return `${hour}:${min}:${sec}.${ms}`;
 }
 
 
